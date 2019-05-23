e11b593bd6005f5a197e909d97c588e8456d0945
Eduardo Corrêa <tavaresdu@gmail.com>
2019-05-23 20:04:27 -0300

    Implementing functions

--- julius.py
+++ julius.py
@@ -14,22 +14,34 @@ TOKEN = os.environ['CODENATION_TOKEN']
 FILENAME = 'answer.json'
 
 def main():
-    json_obj = get_json_from_api()
-    json_obj['decifrado'] = decipher_text(json_obj['cifrado'])
-    json_obj['resumo_criptografico'] = calculate_hash(json_obj['decifrado'])
-    save_json_to_file(json_obj)
+    j = get_json_from_api()
+    j['decifrado'] = decipher_text(j['cifrado'], j['numero_casas'])
+    j['resumo_criptografico'] = sha1_hash(j['decifrado'])
+    save_json_to_file(j)
 
 def get_json_from_api():
-    pass
+    response = requests.get(URL + TOKEN)
+    return json.loads(response.text)
 
-def decipher_text(ciphered_text):
-    pass
+def decipher_text(ciphered_text, rotation):
+    intab = rotate_text(string.ascii_lowercase, rotation)
+    transtab = str.maketrans(intab, string.ascii_lowercase)
+    return ciphered_text.translate(transtab)
 
-def calculate_hash(text):
-    pass
+def rotate_text(text, rotation):
+    lst = list(text)
+    rotation = rotation % len(lst)
+    lst = lst[rotation:] + lst[:rotation]
+    return ''.join(lst)
+
+def sha1_hash(text):
+    h = hashlib.sha1(text.encode('UTF-8'))
+    return h.hexdigest()
 
 def save_json_to_file(json_obj):
-    pass
+    json_str = json.dumps(json_obj)
+    with open(FILENAME, 'w') as f:
+        f.write(json_str)
 
 if __name__ == '__main__':
     main()
